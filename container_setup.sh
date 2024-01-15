955a3e8036fe72a44b5911e1951bc4ae4a56cf45
Alex <a.pferdmenges@gmail.com>
2024-01-16 00:41:20 +0100

    More fixes

--- container_setup.sh
+++ container_setup.sh
@@ -4,7 +4,7 @@
 apt update -y && apt upgrade -y && apt install curl iptables iptables-persistent -y
 
 # Install zerotier
-curl -s https://install.zerotier.com | sudo bash
+curl -s https://install.zerotier.com | bash
 
 # Join network
 zerotier-cli join $1
@@ -12,7 +12,7 @@ zerotier-cli join $1
 # Setup iptables rules
 PHY_IFACE=eth0; ZT_IFACE=zt7nnig26
 
-itables p-t nat -A POSTROUTING -o $PHY_IFACE -j MASQUERADE
+iptables p-t nat -A POSTROUTING -o $PHY_IFACE -j MASQUERADE
 iptables -A FORWARD -i $PHY_IFACE -o $ZT_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i $ZT_IFACE -o $PHY_IFACE -j ACCEPT
 
